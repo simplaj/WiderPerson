9fcdbdbe304dcafa6cca660d2b60280b414bf176
tzh <zhihuatean@gmail.com>
2023-12-20 07:42:07 +0000

    add wandb project name

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
-from ultralytics import settings
 from ultralytics import YOLO
 
 
-settings.update({'datasets_dir': './data'})
+if __name__ == '__main__':
+    model = YOLO('yolov8n.pt')
 
-model = YOLO('yolov8n.pt')
-
-results = model.train(data='WiderPerson.yaml', epochs=100, imgsz=640)
+    results = model.train(
+        data='WiderPerson.yaml',
+        epochs=100, 
+        imgsz=640, 
+        batch=8,
+        project='WiderPerson',
+        )
