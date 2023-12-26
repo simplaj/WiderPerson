245caa2240e2a994e7fd57b88512806d429f7d8d
tzh <zhihuatean@gmail.com>
2023-12-26 08:05:38 +0000

    1000 sz s model

--- train.py
+++ train.py
@@ -2,13 +2,13 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('yolov8n.pt')
+    model = YOLO('yolov8s.pt')
 
     results = model.train(
         data='WiderPerson.yaml',
-        epochs=100, 
-        imgsz=(1000), 
-        batch=4,
+        epochs=400, 
+        imgsz=1000, 
+        batch=2,
         save_period=10,
         project='WiderPerson',
         )
